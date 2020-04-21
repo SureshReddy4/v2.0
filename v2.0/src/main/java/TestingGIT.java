3da0972ee816b3f11ddbf6eabf5cff7d84eb5239
suresh.b <suresh.b@DESKTOP-EBE5TQA.infionic.com>
2020-04-21 18:45:38 +0530

    Print Statement Added

--- v2.0/src/main/java/TestingGIT.java
+++ v2.0/src/main/java/TestingGIT.java
@@ -1,9 +1,9 @@
 
 public class TestingGIT {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
 	}
 
 }
